79b7231cd8248ffd267158d7752e076efb6e609f
Ong Eng Kheng <ongengkheng929@gmail.com>
2022-04-24 22:51:30 +0800

    feat:improve stat test for survey object

--- tickcounter/survey/survey.py
+++ tickcounter/survey/survey.py
@@ -1,5 +1,7 @@
 import numpy as np
 import seaborn as sns
+import pandas as pd
+
 from ..util import plot_each_col
 from tickcounter import statistics, plot
 from tickcounter.config import *
@@ -32,6 +34,7 @@ class Survey(object):
         return statistics._compute_eta_squared(self, *args)
     
     def compare_mean(self, num_col, group_col, *, cohen_es=COHEN_ES, eta=ETA, p_value=P_VALUE, min_sample=MIN_SAMPLE):
+        # TODO: Do we want to expose this method? Because it return None when nothing happens
         return statistics._compare_mean(self.data, 
                                         num_col, 
                                         group_col, 
@@ -48,13 +51,27 @@ class Survey(object):
                                          phi_es=phi_es,
                                          min_sample=min_sample)
 
-    def t_test(self, num_col, group_col, group_1, group_2, **kwargs):
-        return statistics._t_test(data=self.data,
-                                  num_col=num_col,
-                                  group_col=group_col,
-                                  group_1=group_1,
-                                  group_2=group_2,
-                                  **kwargs)
+    def t_test(self, num_col, group_col, group_1=None, group_2=None, **kwargs):
+        if group_1 is None and group_2 is None:
+            groups = self.data[group_col].value_counts()
+            if len(groups) != 2:
+                raise ValueError(f"Column {group_col} has more than 2 groups")
+            else:
+                return statistics._t_test(data=self.data,
+                                          num_col=num_col,
+                                          group_col=group_col,
+                                          group_1=groups.index[0],
+                                          group_2=groups.index[1],
+                                          **kwargs)
+        elif group_1 is not None and group_2 is not None:
+            return statistics._t_test(data=self.data,
+                                      num_col=num_col,
+                                      group_col=group_col,
+                                      group_1=group_1,
+                                      group_2=group_2,
+                                      **kwargs)
+        else:
+            raise ValueError("Please specify both group_1 and group_2")
     
     def compute_cohen_es(self, sample_1, sample_2):
         return statistics._compute_cohen_es(sample_1, sample_2)
@@ -65,7 +82,13 @@ class Survey(object):
     def chi_squared(self, col_1, expected=None):
         return statistics._chi_squared(self.data, col_1, expected)
     
-    def chi_squared_dependence(self, col_1, col_2, groups_1, groups_2):
+    def chi_squared_dependence(self, col_1, col_2, groups_1=None, groups_2=None, min_sample=MIN_SAMPLE):
+        if groups_1 is None:
+            filtered, ignored = statistics._filter_sparse_group(self.data, col_1, min_sample)
+            groups_1 = filtered
+        if groups_2 is None:
+            filtered, ignored = statistics._filter_sparse_group(self.data, col_2, min_sample)
+            groups_2 = filtered
         return statistics._chi_squared_dependence(self.data, col_1, col_2, groups_1, groups_2)
 
     def hist_num(self, **kwargs):
